e4a079512c63e2814791b2c1ab8dd48b19ecfed3
yauheni <ybutski@gmail.com>
2022-09-19 12:51:42 +0300

    Fix one error

--- src/components/MonacoThemeCodeEditor/editorErrors/EditorErrors.styles.ts
+++ src/components/MonacoThemeCodeEditor/editorErrors/EditorErrors.styles.ts
@@ -27,7 +27,7 @@ export const useStyles = makeStyles((theme: Theme) =>
       marginBottom: theme.spacing(),
     },
     errorLine: {
-      fontWeight: theme.typography.fontWeightBold,
+      fontWeight: 900,
     },
     expandIcon: {
       transition: theme.transitions.create('transform'),
